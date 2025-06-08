2f6e9471efc9aa22e246e2892efab1a21ac9cb0c
Sagar Hirulkar <166131762+sagarrrrrrrr@users.noreply.github.com>
2025-06-08 18:40:01 +0530

    Create youtube_uploader.py

---
+++ youtube_uploader.py
@@ -0,0 +1,46 @@
+# youtube_uploader.py
+import os
+import pickle
+from google.oauth2.credentials import Credentials
+from google_auth_oauthlib.flow import InstalledAppFlow
+from googleapiclient.discovery import build
+from googleapiclient.http import MediaFileUpload
+
+SCOPES = ['https://www.googleapis.com/auth/youtube.upload']
+
+
+def upload_video(video_path, title, description):
+    creds = None
+    if os.path.exists('token.pickle'):
+        with open('token.pickle', 'rb') as token:
+            creds = pickle.load(token)
+
+    if not creds or not creds.valid:
+        if creds and creds.expired and creds.refresh_token:
+            creds.refresh(Request())
+        else:
+            flow = InstalledAppFlow.from_client_secrets_file(
+                'client_secret_1044752299120-jaoul3v39ui43fvs7cbsnq1a9io2afvr.apps.googleusercontent.com.json', SCOPES)
+            creds = flow.run_local_server(port=0)
+        with open('token.pickle', 'wb') as token:
+            pickle.dump(creds, token)
+
+    youtube = build('youtube', 'v3', credentials=creds)
+
+    body = {
+        'snippet': {
+            'title': title,
+            'description': description,
+            'tags': ['motivation', 'shorts', 'aesthetic'],
+            'categoryId': '22'  # People & Blogs
+        },
+        'status': {
+            'privacyStatus': 'public'
+        }
+    }
+
+    media = MediaFileUpload(video_path, mimetype='video/mp4', resumable=True)
+    request = youtube.videos().insert(part=','.join(body.keys()), body=body, media_body=media)
+    response = request.execute()
+
+    return f"https://youtu.be/{response['id']}"
